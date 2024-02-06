78d1eb48c2856f67784f047ed41bdaa56d7edb9b
tmax818 <tylermaxwell661@gmail.com>
2024-02-06 07:42:49 -0800

    update

--- algorithms/singlyLinkedList/index.js
+++ algorithms/singlyLinkedList/index.js
@@ -81,9 +81,7 @@ class ListNode {
       runner.next = newNode;
       return this;
 
-
-
-
+    
     }
   
     /**
@@ -128,6 +126,26 @@ class ListNode {
       }
       return arr;
     }
+
+      /**
+       * Creates a new node with the given data and inserts that node at the front
+       * of this list.
+       * - Time: (?).
+       * - Space: (?).
+       * @param {any} data The data for the new node.
+       * @returns {SinglyLinkedList} This list.
+       */
+      insertAtFront(data) {}
+
+      /**
+       * Removes the first node of this list.
+       * - Time: (?).
+       * - Space: (?).
+       * @returns {any} The data from the removed node.
+       */
+      removeHead() {}
+
+
   }
   
   /******************************************************************* 
