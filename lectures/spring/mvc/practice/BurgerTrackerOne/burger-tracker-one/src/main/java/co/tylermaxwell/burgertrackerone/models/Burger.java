0a56f3387129a5dee83c82a94258023950695efc
tmax818 <tylermaxwell661@gmail.com>
2024-02-15 12:48:57 -0800

    update bt guide

--- lectures/spring/mvc/practice/BurgerTrackerOne/burger-tracker-one/src/main/java/co/tylermaxwell/burgertrackerone/models/Burger.java
+++ lectures/spring/mvc/practice/BurgerTrackerOne/burger-tracker-one/src/main/java/co/tylermaxwell/burgertrackerone/models/Burger.java
@@ -4,8 +4,8 @@ package co.tylermaxwell.burgertrackerone.models;
 
 public class Burger {
 
-    //all the appropriate fields come from the form
+    //TODO all the appropriate fields come from the form
 
-    // Don't forget to add annotations and getters & setters!
+    //TODO  Don't forget to add annotations and getters & setters!
     
 }
