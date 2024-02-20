7170f5da6468366446214524602151012259f408
tmax818 <tylermaxwell661@gmail.com>
2024-02-20 08:42:55 -0800

    update

--- algorithms/queues/index.js
+++ algorithms/queues/index.js
@@ -15,7 +15,10 @@ class Queue {
      * @param {any} item The new item to add to the back.
      * @returns {number} The new size of this queue.
      */
-    enqueue(item) {}
+    enqueue(item) {
+      this.items.push(item);
+      return this.size();
+    }
   
     /**
      * Removes and returns the first item of this queue.
@@ -24,7 +27,9 @@ class Queue {
      * - Space: O(1) constant.
      * @returns {any} The first item or undefined if empty.
      */
-    dequeue() {}
+    dequeue() {
+      return this.items.shift();
+    }
   
     /**
      * Retrieves the first item without removing it.
@@ -32,7 +37,9 @@ class Queue {
      * - Space: O(1) constant.
      * @returns {any} The first item or undefined if empty.
      */
-    front() {}
+    front() {
+      return this.items[0];
+    }
   
     /**
      * Returns whether or not this queue is empty.
@@ -40,7 +47,10 @@ class Queue {
      * - Space: O(1) constant.
      * @returns {boolean}
      */
-    isEmpty() {}
+    isEmpty() {
+      return this.size() > 0;
+      return this.size() > 0 ? true : false;
+    }
   
     /**
      * Retrieves the size of this queue.
@@ -48,7 +58,66 @@ class Queue {
      * - Space: O(1) constant.
      * @returns {number} The length.
      */
-    size() {}
+    size() {
+      return this.items.length
+    }
   }
   
   /* Rebuild the above class using a linked list instead of an array. */
+
+  const myQ = new Queue();
+
+  class Qnode {
+
+    constructor(data){
+      this.data = data
+      this.next = null
+    }
+  }
+
+  class LLQ {
+
+    constructor(){
+      this.head = null;
+      this.tail = null;
+      this.size = 0;
+    }
+
+    isEmpty(){
+      return this.head === null;
+    }
+
+    enqueue(value){
+      const newNode = new Qnode(value);
+      if(!this.head){
+        this.head = newNode
+        this.tail = newNode
+        this.size++;
+      } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.size++;
+      }
+        return this.size;
+    }
+
+    dequeue(){
+
+      if(this.isEmpty()){
+        return null;
+      }
+
+      if(this.size === 1){
+        this.tail = null;
+      }
+      let oldHead = this.head;
+      this.head = this.head.next;
+      this.size--;
+      return oldHead.data
+    }
+
+  }
+
+  const myQll = new LLQ()
+
+  console.log(myQll.enqueue(1))
