e621f45b9bc03bbda406953d3678011f587330d4
tmax818 <tylermaxwell661@gmail.com>
2024-02-26 07:03:39 -0800

    update dll

--- algorithms/doublyLinkedList/index.js
+++ algorithms/doublyLinkedList/index.js
@@ -53,6 +53,29 @@ class DoublyLinkedList {
     isEmpty() {
       return this.head === null;
     }
+
+
+      /**
+   * Inserts a new node with the given newVal after the node that has the
+   * given targetVal as it's data.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} targetVal The node data to find.
+   * @param {any} newVal Data for the new node.
+   * @returns {boolean} Indicates if the new node was added.
+   */
+  insertAfter(targetVal, newVal) {}
+
+  /**
+   * Inserts a new node with the given newVal before the node that has the
+   * given targetVal as it's data.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} targetVal The node data to find.
+   * @param {any} newVal Data for the new node.
+   * @returns {boolean} Indicates if the new node was added.
+   */
+  insertBefore(targetVal, newVal) {}
   
     /**
      * Converts this list to an array of the node's data.
